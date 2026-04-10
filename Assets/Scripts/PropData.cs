b6f9b4a4671a73dd6e67d6422d3c118f52d8a34a
ITU\shuc <shubhendrachaddha.16@gmail.com>
2026-04-10 15:07:44 +0200

    Added More Filtering

--- Assets/Scripts/PropData.cs
+++ Assets/Scripts/PropData.cs
@@ -27,6 +27,7 @@ public class PropData : ScriptableObject
 
     [SerializeField] private bool _checkOrentation;
     [SerializeField] private bool _limitOnePerRoom;
+    [SerializeField] private bool _limitOnePerSection;
     [SerializeField] private PropPlacement _propPlacement; 
 
     public GameObject Prop => _prop;
@@ -35,5 +36,6 @@ public class PropData : ScriptableObject
 
     public bool CheckOrientation => _checkOrentation;
     public bool LimitOnePerRoom => _limitOnePerRoom;
+    public bool LimitOnePerSection => _limitOnePerSection;
     public PropPlacement Placement => _propPlacement;
 }
